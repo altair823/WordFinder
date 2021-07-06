f18a116397df701bf88622e7e14a7e737c6f5513
altair823 <dlsrks0734@gmail.com>
2021-07-07 00:25:27 +0900

    add utf-8 support

--- presenter/help_presenter.py
+++ presenter/help_presenter.py
@@ -2,20 +2,19 @@
 # The helping script could be writen in .txt, .md.
 class HelpPresenter:
     def __init__(self, filename):
-        self.file = open(filename, 'r')
         self.text = ''
+        with open(filename, 'r', encoding='utf-8') as file:
+            if filename[-4:] == '.txt':
+                self.parse_text(file)
+            elif filename[-3:] == '.md':
+                self.parse_md(file)
+            else:
+                raise Exception('Cannot parse help file, Wrong extension!')
 
-        if filename[-4:] == '.txt':
-            self.parse_text()
-        elif filename[-3:] == '.md':
-            self.parse_md()
-        else:
-            raise Exception('Cannot parse help file, Wrong extension!')
-
-    def parse_md(self):
-        for line in self.file:
+    def parse_md(self, file):
+        for line in file:
             self.text += line
 
-    def parse_text(self):
-        for line in self.file:
+    def parse_text(self, file):
+        for line in file:
             self.text += line
